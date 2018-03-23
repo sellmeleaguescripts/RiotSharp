53c423f925d82dd108ceb0898aa33c21c204805a
RiotSharp <37712733+RiotSharp@users.noreply.github.com>
2018-03-23 16:33:34 -0300

    EloBuddy - SDK, Update 8.6

---
+++ RiotSharp.SDK/Enumerations/HitChance.cs
@@ -0,0 +1,17 @@
+﻿namespace EloBuddy.SDK.Enumerations
+{
+    public enum HitChance
+    {
+        Unknown,
+        Impossible,
+        Collision,
+        Low,
+        AveragePoint,
+        Medium,
+        High,
+        Dashing,
+        Immobile
+    }
+}
+
+//= 8
---
+++ RiotSharp.SDK/Spells/SpellDatabase.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace EloBuddy.SDK.Spells
+{
+    public static class SpellDatabase
+    {
+        internal static Dictionary<string, List<SpellInfo>> Database { get; set; }
+
+        internal static void Initialize()
+        {
+            // Deserialize database
+            Database = JsonConvert.DeserializeObject<Dictionary<string, List<SpellInfo>>>(DefaultSettings.SpellDatabase);
+
+            // Listen to required events
+            //Obj_AI_Base.OnProcessSpellCast += OnProcessSpellCast;
+            //Obj_AI_Base.OnSpellCast += OnSpellCast;
+        }
+
+        public static List<SpellInfo> GetSpellInfoList(string baseSkinName)
+        {
+            if (Database.ContainsKey(baseSkinName))
+            {
+                return Database[baseSkinName];
+            }
+            return new List<SpellInfo>();
+        }
+
+        public static List<SpellInfo> GetSpellInfoList(Obj_AI_Base sender)
+        {
+            return GetSpellInfoList(sender.BaseSkinName);
+        }
+    }
+}
